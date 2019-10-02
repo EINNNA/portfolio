9b5992706abde20b6d246a587f37d97bc957583d
EINNNA <annie_t_@hotmail.com>
2019-10-02 18:13:17 +1000

    changed layout

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,76 +1,25 @@
 //FRONT PAGE
-$(".aboutMeClick2").on("click", function aboutMeClick(){
+$(".aboutMeClick").on("click", function aboutMeClick(){
     console.log("Hello1");
     $(".aboutMePage").removeClass("hide");
     $(".landingWrap").addClass("hide");
 });
 
-$(".githubClick2").on("click", function worksClick(){
+$(".githubClick").on("click", function worksClick(){
     console.log("Hello2");
     $(".githubPage").removeClass("hide");
     $(".landingWrap").addClass("hide");
 });
 
-$(".resumeClick2").on("click", function resumeClick(){
+$(".resumeClick").on("click", function resumeClick(){
     console.log("Hello3");
     $(".resumePage").removeClass("hide");
     $(".landingWrap").addClass("hide");
 });
 
-$(".contactClick2").on("click", function contactClick(){
+$(".contactClick").on("click", function contactClick(){
     console.log("Hello4");
     $(".contactPage").removeClass("hide");
     $(".landingWrap").addClass("hide");    
 });
 
-//NAVIGATION PAGE
-$(".aboutMeClick").on("click", function aboutMeClick(){
-    console.log("Hello1");
-    $(".aboutMePage").removeClass("hide");
-    $(".landingWrap").addClass("hide");
-    $(".resumePage").addClass("hide");
-    $(".githubPage").addClass("hide");
-    $(".contactPage").addClass("hide");
-});
-
-$(".githubClick2").on("click", function worksClick(){
-    console.log("Hello2");
-    $(".aboutMePage").addClass("hide");
-    $(".landingWrap").addClass("hide");
-    $(".resumePage").addClass("hide");
-    $(".githubPage").removeClass("hide");
-    $(".contactPage").addClass("hide");
-});
-
-$(".resumeClick2").on("click", function resumeClick(){
-    console.log("Hello3");
-    $(".aboutMePage").addClass("hide");
-    $(".landingWrap").addClass("hide");
-    $(".resumePage").removeClass("hide");
-    $(".githubPage").addClass("hide");
-    $(".contactPage").addClass("hide");
-});
-
-$(".contactClick2").on("click", function contactClick(){
-    console.log("Hello4");
-    $(".aboutMePage").addClass("hide");
-    $(".landingWrap").addClass("hide");
-    $(".resumePage").addClass("hide");
-    $(".githubPage").addClass("hide");
-    $(".contactPage").removeClass("hide");
-});
-
-
-$(".arrow").on("click", function navigatorArrow(){
-    $(".arrow").html("x");
-    $(".arrow").addClass("x");
-    $(".navigationList").removeClass("hide");
-        
-});
-
-
-//FUNCTION 
-
-$("li").on("click", function hideHeader(){
-    
-});
